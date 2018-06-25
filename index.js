e1ae028d128ade196a5a0331fb2b0aa92ab70710
Cheng Qian <qianch@indiana.edu>
2018-06-25 15:58:27 -0500

    Super Mega Bonus Credit Hyper Fighting

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
+const button = document.querySelector('button')
 
-
-
-/*function change3() {
-    const h1 = document.querySelector('#ALLA')
-    h1.textContent = document.forms.querySelector('#ahead').value
-    return false;
+const change = function() {
+  const h = document.querySelector('h1')
+  h.textContent = 'Wuuu Yeah!'
 }
-
-//', change3 )
-
-document.addEventListener('keypress', (event) => {
-    const keyName = event.key;
-    const result = document.getElementById("ahead").innerHTML;
-    const h1 = document.getElementById('ALLA')
-       if (keyName === 'Enter') {
-        h1.textContent = result.innerHTML
-            ;
-        }
-
-    }
-)
-*/
+button.addEventListener('click', change)
+const change2 = function() {
+    const h = document.querySelector('#h101')
+    h.textContent = 'Greedy is good!'
+  }
+button.addEventListener('click', change2)
 
 const submit = document.querySelector('#i100')
 
@@ -31,10 +20,21 @@ const change1 = function() {
   
 }
 
-
-
 submit.addEventListener('click', change1)
-document.addEventListener("click", function(event) {
-    document.getElementById("i100");
+
+document.getElementById("i100").addEventListener("click", function(event) {
+    
     event.preventDefault();
 }, false);
+
+
+document.addEventListener('keypress', (event) => {
+    const keyName = event.key;
+    const result = document.getElementById("ahead").innerHTML;
+    const h1 = document.getElementById('ALLA')
+       if (keyName === 'Enter') {
+        h1.textContent = result.innerHTML;
+        }
+
+    }
+)
