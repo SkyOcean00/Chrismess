2d5dcb15998205d76db62026e61ef22815a3223d
Cheng Qian <qianch@indiana.edu>
2018-06-25 15:48:42 -0500

    Super Mega Bonus Credit

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
-const button = document.querySelector('button')
+
+
+
+/*function change3() {
+    const h1 = document.querySelector('#ALLA')
+    h1.textContent = document.forms.querySelector('#ahead').value
+    return false;
+}
+
+//', change3 )
+
+document.addEventListener('keypress', (event) => {
+    const keyName = event.key;
+    const result = document.getElementById("ahead").innerHTML;
+    const h1 = document.getElementById('ALLA')
+       if (keyName === 'Enter') {
+        h1.textContent = result.innerHTML
+            ;
+        }
+
+    }
+)
+*/
+
+const submit = document.querySelector('#i100')
+
 
 const change1 = function() {
   const h = document.querySelector('h1')
-  h.textContent = 'Wuuu Yeah!'
+  h.textContent = document.querySelector("#ahead").value
+  
 }
 
-const change2 = function() {
-    const h1 = document.querySelector('#h101')
-    h1.textContent = 'Great is good!'
-  }
 
 
-button.addEventListener('click', change1)
-button.addEventListener('click', change2)
+submit.addEventListener('click', change1)
+document.addEventListener("click", function(event) {
+    document.getElementById("i100");
+    event.preventDefault();
+}, false);
