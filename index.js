0fa840226ca480cbafa18a2aa9f85a48e8b4e0f3
Cheng Qian <qianch@indiana.edu>
2018-06-25 13:46:30 -0500

    Day 1 Homework
    Make the button change the heading (the '<h1>')

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('It works')
+const button = document.querySelector('button')
+
+const change1 = function() {
+  const h = document.querySelector('h1')
+  h.textContent = 'Wuuu Yeah!'
+}
+
+const change2 = function() {
+    const h = document.querySelector('#h101')
+    h.textContent = 'Greedy is good!'
+  }
+
+
+button.addEventListener('click', change1)
+//button.addEventListener('click', change2)
