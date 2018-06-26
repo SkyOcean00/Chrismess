bd11e09aaff12aa1adde40f1bfd2fdbd4dfbfa80
Cheng Qian <qianch@indiana.edu>
2018-06-26 13:57:59 -0500

    Bonus Credit.       Display 2nd  field in a separate HTML element

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ const addFlick = function(ev) {
   const year = f.Year.value
   const flickName = f.flickName.value
   const item = document.createElement('li')
-  item.textContent = flickName
-  item.textContent +=' '+year
+  const s1 = document.createElement('span')
+  const s2 = document.createElement('span')
+
+  s1.textContent = flickName
+  s2.textContent =' '+year
+
+  item.appendChild(s1)
+  item.appendChild(s2)
+  
 
   const list = document.querySelector('#flicks')
   list.appendChild(item)
